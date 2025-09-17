fb40f907886ccb5603042947d17d371c209861f5
Junmo <hongjm1022@gmail.com>
2025-09-17 16:02:57 +0900

    chore: Swagger setting update

--- src/main/kotlin/siu/siubackend/common/config/OpenApiConfig.kt
+++ src/main/kotlin/siu/siubackend/common/config/OpenApiConfig.kt
@@ -2,6 +2,7 @@ package siu.siubackend.common.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration
 class OpenApiConfig {
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
+        .servers(listOf(Server().url("/")))
         .info(
             Info()
                 .title("SIU Backend API")
