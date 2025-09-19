ab5709083d035cd51b8c332b9fa56a8ad2ee273a
Junmo <hongjm1022@gmail.com>
2025-09-19 21:30:49 +0900

    feat: orderstatusrequest jsonnaming delete

--- src/main/kotlin/siu/siubackend/order/adapter/in/dto/UpdateOrderStatusRequest.kt
+++ src/main/kotlin/siu/siubackend/order/adapter/in/dto/UpdateOrderStatusRequest.kt
@@ -1,10 +1,7 @@
 package siu.siubackend.order.adapter.`in`.dto
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import siu.siubackend.order.domain.OrderStatus
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UpdateOrderStatusRequest(
     val status: OrderStatus
 )
