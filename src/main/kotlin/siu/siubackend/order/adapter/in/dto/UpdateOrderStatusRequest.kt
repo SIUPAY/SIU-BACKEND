5203b5422db08d1122e6728b388f620d34d11055
CHANGWOO JUNG <changwoojung@CHANGWOOui-MacBookAir.local>
2025-09-19 21:54:13 +0900

    fix: jackson deserialize error

--- src/main/kotlin/siu/siubackend/order/adapter/in/dto/UpdateOrderStatusRequest.kt
+++ src/main/kotlin/siu/siubackend/order/adapter/in/dto/UpdateOrderStatusRequest.kt
@@ -1,7 +1,9 @@
 package siu.siubackend.order.adapter.`in`.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import siu.siubackend.order.domain.OrderStatus
 
-data class UpdateOrderStatusRequest(
-    val status: OrderStatus
+data class UpdateOrderStatusRequest @JsonCreator constructor(
+    @JsonProperty("status") val status: OrderStatus
 )
